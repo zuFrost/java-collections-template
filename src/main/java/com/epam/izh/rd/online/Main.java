8ddbf6f06a56f3122ee5319e14169de1d096f283
Alex Frost <zufrost.a@gmail.com>
2020-05-05 12:00:25 +0300

    make branch and test FileReaderService class

--- src/main/java/com/epam/izh/rd/online/Main.java
+++ src/main/java/com/epam/izh/rd/online/Main.java
@@ -1,11 +1,17 @@
 package com.epam.izh.rd.online;
 
+import com.epam.izh.rd.online.helper.FileReaderService;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
+        FileReaderService fileReaderService = new FileReaderService();
+        System.out.println(fileReaderService.readFromFileToString("sample-text.txt"));
+
+
 
     }
 }
